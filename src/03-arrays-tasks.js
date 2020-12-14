25d6ed579c241bf226b34918b665098dcc2249bd
Nickieros <>
2020-12-14 16:07:22 +0200

    feat: arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -476,8 +476,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
@@ -510,8 +510,13 @@ function distinct(/* arr */) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((acc, pair) => {
+    const currentKey = keySelector(pair);
+    if (!acc.has(currentKey)) acc.set(currentKey, []);
+    acc.get(currentKey).push(valueSelector(pair));
+    return acc;
+  }, new Map());
 }
 
 
